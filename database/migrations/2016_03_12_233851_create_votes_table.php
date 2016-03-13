a2d907ce1e1c433a23f4dd78b8e6cf4fb102acea
ALLIAUME Hugo <kocal@live.fr>
2016-03-13 01:00:17 +0100

    Ajout des migrations

---
+++ database/migrations/2016_03_12_233851_create_votes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('video_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                  ->onDelete('cascade');
+
+            $table->foreign('video_id')->references('id')->on('videos')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('votes');
+    }
+}
