b26238ea7c46d19c127e59b4b8d17ae175025e8e
ALLIAUME Hugo <kocal@live.fr>
2016-03-21 21:52:07 +0100

    Ajout titre pour la page + order des div[class^=col]

--- resources/views/pages/video/add.blade.php
+++ resources/views/pages/video/add.blade.php
@@ -1,21 +1,23 @@
-@extends('layouts.default')
+@extends('layouts.default', [
+    'title' => 'Ajouter une vidéo | Amarantia.fr'
+])
 
 @section('content')
-    {!! BootForm::open() !!}
-
-    <div class="col-md-5">
-        <h2>Aperçu</h2>
-        <div class="embed-responsive embed-responsive-16by9">
-            <video controls autoplay muted></video>
-        </div>
-    </div>
-    <div class="col-md-7">
+    {!! BootForm::open()->enctype('multipart/form-data') !!}
+    <div class="col-md-6 col-md-push-6">
         <h2>Ajouter une vidéo</h2>
         {!! BootForm::file('Vidéo (webm, mp4)', 'video')->accept('video/webm,video/mp4')->class('video-picker') !!}
         {!! BootForm::text("Titre", 'title') !!}
         {!! BootForm::textarea("Description", 'description') !!}
         {!! BootForm::submit("<i class='fa fa-btn fa-upload'></i> Envoyer", 'btn btn-primary') !!}
         {!! BootForm::close() !!}
+    </div>
+
+    <div class="col-md-6 col-md-pull-6">
+        <h2>Aperçu</h2>
+        <div class="embed-responsive embed-responsive-16by9">
+            <video controls autoplay muted></video>
+        </div>
     </div>
 
     {!! BootForm::close() !!}
