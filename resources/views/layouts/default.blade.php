1c601c97008657e799cbbd85765f3f03503772bf
ALLIAUME Hugo <kocal@live.fr>
2016-03-21 21:13:40 +0100

    Ajout d'un @yield pour chager le JS appelé dans une vue

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -74,5 +74,6 @@
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+@yield('js')
 </body>
 </html>
