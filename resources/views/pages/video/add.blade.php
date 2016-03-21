ef3844f67bfa49241f89336037aeb1703e468c6d
ALLIAUME Hugo <kocal@live.fr>
2016-03-21 22:17:17 +0100

    Un BootForm::close() s'était perdu

--- resources/views/pages/video/add.blade.php
+++ resources/views/pages/video/add.blade.php
@@ -4,13 +4,13 @@
 
 @section('content')
     {!! BootForm::open()->enctype('multipart/form-data') !!}
+
     <div class="col-md-6 col-md-push-6">
         <h2>Ajouter une vidéo</h2>
         {!! BootForm::file('Vidéo (webm, mp4)', 'video')->accept('video/webm,video/mp4')->class('video-picker') !!}
         {!! BootForm::text("Titre", 'title') !!}
         {!! BootForm::textarea("Description", 'description') !!}
         {!! BootForm::submit("<i class='fa fa-btn fa-upload'></i> Envoyer", 'btn btn-primary') !!}
-        {!! BootForm::close() !!}
     </div>
 
     <div class="col-md-6 col-md-pull-6">
